aa835896966be6319230fec062df8974e9e0fae5
Augusto Szymberg <129625203+Augusto-Szymberg@users.noreply.github.com>
2023-04-18 19:25:09 -0300

    Create app.js

---
+++ WebsiteDEMO/static/app.js
@@ -0,0 +1,34 @@
+const chatForm = document.getElementById("chat-form");
+const chatbox = document.getElementById("chatbox");
+const input = document.getElementById("input");
+
+chatForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const message = input.value.trim();
+    if (!message) return;
+
+    addMessageToChatbox(`You: ${message}`);
+    input.value = "";
+    input.focus();
+
+    const response = await fetch("/api/chat", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ prompt: message })
+    });
+
+    const responseText = await response.text();
+    addMessageToChatbox(`ChatGPT: ${responseText}`);
+});
+
+function addMessageToChatbox(message) {
+    const messageElement = document.createElement("div");
+    messageElement.textContent = message;
+    messageElement.className = "message";
+    chatbox.appendChild(messageElement);
+
+    chatbox.scrollTop = chatbox.scrollHeight;
+}
